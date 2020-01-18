0e951ddccff4ac299089e4ce1988e67654b9ee1d
Vitor Henrique <vitorhenriquec@users.noreply.github.com>
2020-01-17 21:27:13 -0300

    Update to JpaRepository

--- src/main/java/br/com/todo/repository/TaskRepository.java
+++ src/main/java/br/com/todo/repository/TaskRepository.java
@@ -1,9 +1,11 @@
 package br.com.todo.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.todo.domain.Task;
 
-public interface TaskRepository extends CrudRepository<Task, Long>{
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long>{
 
 }
